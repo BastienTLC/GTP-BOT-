948bf29d0843598f0742560d8365a733a0443aa9
Goupil29Super <63251085+Goupil29Super@users.noreply.github.com>
2021-04-10 10:28:27 +0200

    init

---
+++ Modules/Commands.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+
+
+
+using Image = SixLabors.ImageSharp.Image;
+
+namespace GlisseTonPiedBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("giroud")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("https://cdn.discordapp.com/attachments/732571903729860695/829710528498761758/isgiroud.mp4");
+        }
+
+        [Command("fouine")]
+        public async Task AvatarAsync(SocketUser user = null, ushort size = 512)
+        {
+            if (user == null)
+            {
+                await ReplyAsync(CDN.GetUserAvatarUrl(Context.User.Id, Context.User.AvatarId, size, ImageFormat.Auto));
+            }
+            else
+            {
+                await ReplyAsync(CDN.GetUserAvatarUrl(user.Id, user.AvatarId, size, ImageFormat.Auto));
+               // await ReplyAsync(user.GetAvatarUrl());
+            }
+        }
+
+        [Command("brignness")]
+        public async Task DistortionAvatar(SocketUser user = null, ushort size = 512)
+        {
+            EditImage editImage = new EditImage(CDN.GetUserAvatarUrl(user.Id, user.AvatarId, size, ImageFormat.Auto));
+            Console.WriteLine(user.GetAvatarUrl());
+            editImage.Brightness();
+            
+
+            await Context.Channel.SendFileAsync("image\\update.png", "Caption goes here");
+
+        }
+
+        [Command("text")]
+        public async Task TextAvatar(SocketUser user = null,String text = "TEXT")
+        {
+            EditImage editImage = new EditImage(CDN.GetUserAvatarUrl(user.Id, user.AvatarId, 512, ImageFormat.Auto));
+            Console.WriteLine(user.GetAvatarUrl());
+            editImage.writeMessageOnImage(text);
+
+
+            await Context.Channel.SendFileAsync("image\\update.png", "Caption goes here");
+
+        }
+
+
+    }
+}
---
+++ Modules/EditImage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+
+using Image = SixLabors.ImageSharp.Image;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.Fonts;
+
+namespace GlisseTonPiedBot.Modules
+{
+    class EditImage
+    {
+        private String imageLink;
+
+        public EditImage(string imageLink)
+        {
+            this.imageLink = imageLink;
+        }
+
+        public String convertWebImage()
+        {
+            String fillChemin = "image/imgtmp.png";
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFile(imageLink, fillChemin);
+                Console.WriteLine("DL de " + imageLink);
+            }
+            return fillChemin;
+        }
+
+        public String Brightness()
+        {
+            String EditFillChemin = "image / update.png";
+            using (Image image = Image.Load(convertWebImage()))
+            {
+                image.Mutate(c => c.ProcessPixelRowsAsVector4(row =>
+                {
+                    for (int x = 0; x < row.Length; x++)
+                    {
+                        // We can apply any custom processing logic here
+                        row[x] = Vector4.SquareRoot(row[x]);
+                    }
+                }));
+                image.Save("image/update.png");
+                // image.Save(outPath);
+                return EditFillChemin;
+            }
+        }
+
+        public String writeMessageOnImage(String text)
+        {
+            String EditFillChemin = "image / update.png";
+            
+
+            FontCollection collection = new FontCollection();
+            FontFamily family = collection.Install("font/arial.ttf");
+            
+
+            using (Image image = Image.Load(convertWebImage()))
+            {
+
+                int fontSize = 1;
+                
+
+                while ((image.Width)/((text.Length)* fontSize) > 0)
+                {
+                    fontSize++;
+                    Console.WriteLine(fontSize);
+                }
+                
+                    Font font = family.CreateFont(fontSize*1, FontStyle.Italic);
+
+                image.Mutate(x => x.DrawText(text, font, Color.Black, new PointF(10, 10)));
+
+                image.Save("image/update.png");
+                // image.Save(outPath);
+                return EditFillChemin;
+            }
+        }
+
+
+    }
+}
